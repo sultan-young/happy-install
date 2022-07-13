30fdb242425ed735363ebc02ff36cc611ec35acd
yangzilong <yangzilong@hungrypandagroup.com>
2022-07-13 18:22:14 +0800

    feat: 优化commit-lint

--- src/commit-lint/start.js
+++ src/commit-lint/start.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const shell = require("shelljs")
-const packageJson = require('../../package.json');
-const commitLintTemplate = require('./.commitlintrc.js');
 
 function generateCommitLint() {
     const result = shell.exec('git rev-parse --is-inside-work-tree', {
@@ -19,10 +17,12 @@ function generateCommitLint() {
     shell.ls('.husky/commit-msg').forEach(file => {
         shell.sed('-i', 'undefined', "npx --no-install commitlint --edit ''", file)
     })
+    const packageJson = JSON.parse(fs.readFileSync('package.json').toString());
     packageJson.scripts.prepare = 'husky install';
     fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
     fs.copyFileSync(path.resolve(__dirname, '.commitlintrc.js'), '.commitlintrc.js')
     shell.exec('npm install');
+    shell.echo('✅ 安装完成!')
 }
 
 module.exports = generateCommitLint;
