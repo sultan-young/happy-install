63e2fd6c0ec3235a0dcc78c44cda29397a85bdbf
yangzilong <yangzilong@hungrypandagroup.com>
2022-07-13 18:12:26 +0800

    feat: 完成happy-commit-lint install

---
+++ src/commit-lint/start.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const shell = require("shelljs")
+const packageJson = require('../../package.json');
+const commitLintTemplate = require('./.commitlintrc.js');
+
+function generateCommitLint() {
+    const result = shell.exec('git rev-parse --is-inside-work-tree', {
+        silent: true
+    });
+    if (!result.stdout) {
+        shell.echo('❌ 当前仓库未在git管理控制下，请先执行git init')
+        shell.exit();
+    }
+    shell.echo('正在安装commit-lint及其相关内容');
+    shell.exec('npm install @commitlint/config-conventional @commitlint/cli husky -D')
+    shell.exec('npx husky install');
+    shell.exec(`npx husky add .husky/commit-msg`);
+    shell.ls('.husky/commit-msg').forEach(file => {
+        shell.sed('-i', 'undefined', "npx --no-install commitlint --edit ''", file)
+    })
+    packageJson.scripts.prepare = 'husky install';
+    fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+    fs.copyFileSync(path.resolve(__dirname, '.commitlintrc.js'), '.commitlintrc.js')
+    shell.exec('npm install');
+}
+
+module.exports = generateCommitLint;
